fa3f0f75b41c714e9ace938f1b58f9a4c5be5f35
Lam H. Dao <daohailam@yahoo.com>
2015-10-17 14:23:52 -0400

    Moved valloc buffer code as shared code

---
+++ buffer.h
@@ -0,0 +1,12 @@
+#ifndef __CAMERA_BUFFER_H
+#define __CAMERA_BUFFER_H
+//----------------------------------------------------------------------------
+template<typename T>
+using buffer_ptr = std::unique_ptr<T,std::function<void(T*)>>;
+//----------------------------------------------------------------------------
+static inline buffer_ptr<uchar> CreateBuffer(int bsize)
+{
+	return buffer_ptr<uchar>((uchar*)valloc(bsize), [](uchar *b) {free(b);});
+}
+//----------------------------------------------------------------------------
+#endif
